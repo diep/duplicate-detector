3fd063af515103e006101e2b83f668d60a3a8dcf
Do Hoai Khanh Vu <vudhk@magik.vn>
2016-12-05 08:52:02 +0700

    Tester

---
+++ Tester/CheckOutput.cs
@@ -0,0 +1,196 @@
+﻿
+using DuplicateDetectorUWP.Detector.Enumerable;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester
+{
+    class ModelOutput
+    {
+        public ModelOutput()
+        {
+            numOfInput = 0;
+            folderPaths = new List<string>();
+            criteral = new List<EnumerableCompareType>();
+            fileType = new List<string>();
+            numOfOutput = 0;
+            detectOrigin = new List<EnumerableDetectOrigin>();
+            group = new List<object>();
+
+        }
+
+        public int numOfInput { get; set; }
+        public List<String> folderPaths { get; set; }
+        public List<EnumerableCompareType> criteral { get; set; }
+        public List<string> fileType { get; set; }
+        public List<EnumerableDetectOrigin> detectOrigin { get; set; }
+        public int numOfOutput { get; set; }
+        public List<object> group { get; set; }
+    }
+
+    internal class CheckOutput
+    {
+        /*
+        internal static bool CompareContent(string path1, string path2)
+        {
+            ModelOutput output1 = ReadFile(path1);
+            ModelOutput output2 = ReadFile(path2);
+
+            if (output1.numOfInput != output2.numOfInput
+                || output1.numOfOutput != output2.numOfOutput
+                || output1.criteral != output2.criteral
+                || output1.detectOrigin != output2.detectOrigin
+                || output1.fileType != output2.fileType
+                || output1.copyFilePaths.Count != output2.copyFilePaths.Count
+                || output1.originFilePaths.Count != output2.originFilePaths.Count
+                || output1.folderPaths.Count != output2.folderPaths.Count)
+            {
+                return false;
+            }
+
+            if (CompareList(output1.folderPaths, output2.folderPaths)
+                && CompareList(output1.originFilePaths, output2.originFilePaths)
+                && CompareList(output1.copyFilePaths, output2.copyFilePaths))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        
+        private static bool CompareList(List<String> list1, List<String> list2)
+        {
+            foreach (var i in list1)
+            {
+                if (!list2.Contains(i))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        */
+        internal static ModelOutput ReadFile(string path)
+        {
+            ModelOutput output1 = new ModelOutput();
+            using (Stream stream = File.OpenRead(path))
+            {
+                using (StreamReader stream1 = new StreamReader(stream, Encoding.UTF8))
+                {
+                    //intput 
+                    var b = stream1.ReadLine();
+                    output1.numOfInput = int.Parse(b.Split(':')[1].Trim());
+                    for (int i = 0; i < output1.numOfInput; i++)
+                    {
+                        output1.folderPaths.Add(stream1.ReadLine().Trim());
+                    }
+                    stream1.ReadLine();
+
+                    //option
+                    var criteral = new object();
+                    var buff = stream1.ReadLine().Split('=')[1].Trim().Split(',');
+                    foreach (var i in buff)
+                    {
+                        switch (i)
+                        {
+                            case "Content":
+                                criteral = EnumerableCompareType.Content;
+                                break;
+                            case "Name":
+                                criteral = EnumerableCompareType.Name;
+                                break;
+                            case "Size":
+                                criteral = EnumerableCompareType.Size;
+                                break;
+                            case "DateCreated":
+                                criteral = EnumerableCompareType.DateCreated;
+                                break;
+                            case "DateModified":
+                                criteral = EnumerableCompareType.DateModified;
+                                break;
+                            default:
+                                throw new Exception("Testcase failed!\r\nPath: " + path + "\r\nCriteral: " + i);
+                        }
+                        output1.criteral.Add((EnumerableCompareType)criteral);
+                    }
+
+                    // file type
+                    var fileType = new object();
+                    buff = stream1.ReadLine().Split('=')[1].Trim().Split(',');
+                    foreach (var i in buff)
+                    {
+                        output1.fileType.Add(i);
+                    }
+
+                    //detectOrigin
+                    var detectOrigin = new object();
+                    buff = stream1.ReadLine().Split('=')[1].Trim().Split(',');
+                    foreach (var i in buff)
+                    {
+                        switch (i)
+                        {
+                            case "OldestFile":
+                                detectOrigin = EnumerableDetectOrigin.OldestFile;
+                                break;
+                            case "NewestFile":
+                                detectOrigin = EnumerableDetectOrigin.NewestFile;
+                                break;
+                            case "SmallestSize":
+                                detectOrigin = EnumerableDetectOrigin.SmallestSize;
+                                break;
+                            case "LargestSize":
+                                detectOrigin = EnumerableDetectOrigin.LargestSize;
+                                break;
+                            case "LongestName":
+                                detectOrigin = EnumerableDetectOrigin.LongestName;
+                                break;
+                            case "ShortestName":
+                                detectOrigin = EnumerableDetectOrigin.ShortestName;
+                                break;
+                            default:
+                                throw new Exception("Testcase failed!\r\nPath: " + path + "\r\nDetectOrigin: " + i);
+                        }
+                        output1.detectOrigin.Add((EnumerableDetectOrigin)detectOrigin);
+                    }
+
+                    stream1.ReadLine();
+
+                    //output
+                    output1.numOfOutput = int.Parse(stream1.ReadLine().Split(':')[1].Trim());
+                    for (int i = 0; i < output1.numOfOutput; i++)
+                    {
+                        string buffer = "";
+                        List<string> originFilePaths = new List<string>();
+                        List<string> copyFilePaths = new List<string>();
+
+                        while ((buffer = stream1.ReadLine()) != "")
+                        {
+                            if (buffer == null)
+                                break;
+                            if (buffer.ElementAt(0) == '0')
+                            {
+                                originFilePaths.Add(buffer.Remove(0,2));
+                            }
+                            else
+                            {
+                                copyFilePaths.Add(buffer.Remove(0,2));
+                            }
+                        }
+                        List<List<string>> t = new List<List<string>>();
+                        t.Add(originFilePaths);
+                        t.Add(copyFilePaths);
+                        output1.group.Add(t);
+                    }
+                    //end
+                    stream1.Dispose();
+                }
+                stream.Dispose();
+            }
+            return output1;
+        }
+    }
+}
---
+++ Tester/MainPage.xaml.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using DuplicateDetectorUWP.Detector;
+using Windows.Storage;
+using DuplicateDetectorUWP.Detector.Enumerable;
+using System.Collections.ObjectModel;
+using Windows.UI.Popups;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.ApplicationModel.Core;
+using System.Diagnostics;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Tester
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        ObservableCollection<StorageFolder> Folders;
+        ObservableCollection<GroupRecord> group;
+        DuplicateDetector d;
+        string outputFileName;
+        EnumerableDetectOrigin[] detectOriginOption;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+            d = new DuplicateDetector();
+            outputFileName = String.Empty;
+            Folders = new ObservableCollection<StorageFolder>();
+            group = new ObservableCollection<GroupRecord>();
+
+
+            detectOriginOption = new EnumerableDetectOrigin[] 
+            {
+                EnumerableDetectOrigin.NewestFile,
+                EnumerableDetectOrigin.LargestSize,
+                EnumerableDetectOrigin.ShortestName
+            };
+            cbCompareBy.ItemsSource = new List<EnumerableCompareType>()
+            {
+                EnumerableCompareType.Content,
+                EnumerableCompareType.DateCreated,
+                EnumerableCompareType.DateModified,
+                EnumerableCompareType.Name,
+                EnumerableCompareType.Size
+            };
+            cbCompareBy.SelectedIndex = 0;
+
+            cbCriteral.ItemsSource = new List<EnumerableDetectOrigin>()
+            {
+                EnumerableDetectOrigin.LargestSize,
+                EnumerableDetectOrigin.LongestName,
+                EnumerableDetectOrigin.NewestFile,
+                EnumerableDetectOrigin.OldestFile,
+                EnumerableDetectOrigin.ShortestName,
+                EnumerableDetectOrigin.SmallestSize
+            };
+            cbCriteral.SelectedIndex = 2;
+
+
+            //Copy TestcacseDuplicate folder into LocalSetting and RunTest().
+            //The Result file is result final of this testcase.
+            //RunTest();
+        }
+
+
+        async void RunTest()
+        {
+            var folder = ApplicationData.Current.LocalFolder;
+            var TestcaseDublicateFolder = await folder.GetFolderAsync("TestcaseDublicate");
+            var testcaseFolder = await TestcaseDublicateFolder.GetFolderAsync("TestCase");
+            var fileTests = await testcaseFolder.GetFilesAsync();
+            
+            //create file result
+            var resultFile = await TestcaseDublicateFolder.CreateFileAsync("Result.txt", CreationCollisionOption.ReplaceExisting);
+            var stream = new StreamWriter(File.OpenWrite(resultFile.Path)); //await resultFile.OpenAsync(FileAccessMode.ReadWrite);
+
+            var list = new List<StorageFile>(fileTests);
+            foreach(var file in fileTests)
+            {
+                var c = CheckOutput.ReadFile(file.Path);
+
+                var tmpList = new ObservableCollection<StorageFolder>();
+                try
+                {
+                    foreach (var item in c.folderPaths)
+                    {
+                        tmpList.Add(await StorageFolder.GetFolderFromPathAsync(Path.Combine(folder.Path, item)));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+                d.folderPaths = tmpList;
+                d.CompareBy = c.criteral.ToArray();
+                d.FileTypeFilter = c.fileType.ToArray();
+                d.CryptographyType = EnumerableCryptographType.Md5;
+                detectOriginOption = c.detectOrigin.ToArray();
+
+                Group = await Task.Run(() => d.Execute());
+                d.DetectOriginRecords(Group, detectOriginOption);
+
+                var result = new ModelOutput();
+                result.numOfOutput = Group.Count();
+                result.group = new List<object>(Group);
+
+
+                //check
+                if(result.numOfOutput != c.numOfOutput)
+                {
+                     stream.WriteLine(String.Format("{0}: {1}", file.Name, false));
+                }
+                else
+                {
+                    try
+                    {
+                        foreach (List<List<string>> output in c.group)
+                        {
+                            foreach (GroupRecord records in result.group)
+                            {
+                                var x = false;
+                                foreach (Record rc in records.records)
+                                {
+                                    if (rc.Path.Equals(Path.Combine(folder.Path, output[0][0])))
+                                    {
+                                        check2Group(output, records);
+                                        x = true;
+                                        break;
+                                    }
+                                }
+                                if (x == true)
+                                    break;
+                            }
+                        }
+                        stream.WriteLine(String.Format("{0}: {1}", file.Name, true));
+                    }
+                    catch (Exception ex)
+                    {
+                        if(ex.Message != "False")
+                        {
+                            Debug.WriteLine(file.Name + ex);
+                        }
+                        else
+                        {
+                            stream.WriteLine(String.Format("{0}: {1}", file.Name, false));
+                        }
+                    }
+                }
+            }
+            stream.Dispose();
+            Application.Current.Exit();
+        }
+
+        private void check2Group(List<List<string>> output, GroupRecord records)
+        {
+            List<string> origin = new List<string>();
+            List<string> copy = new List<string>();
+            foreach (var rc in records.records)
+            {
+                var tmp = rc.Path.Split(new string[] { "LocalState" }, StringSplitOptions.RemoveEmptyEntries)[1].Remove(0,1);
+                if (rc.IsOrigin == true)
+                {
+                    origin.Add(tmp);
+                }
+                else
+                {
+                    copy.Add(tmp);
+                }
+            }
+            foreach(var i in origin)
+            {
+                if (!output[0].Contains(i))
+                {
+                    throw new Exception("False");
+                }
+            }
+        }
+
+        private async void btnChooseFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var folderPicker = new Windows.Storage.Pickers.FolderPicker();
+            folderPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
+            folderPicker.FileTypeFilter.Add("*");
+          
+            StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+            if(folder != null && !CheckFolderExist(Folders, folder))
+            {
+                Folders.Add(folder);
+                outputFileName = Folders[0].Name;
+            }
+        }
+
+        private async void btnExecute_Click(object sender, RoutedEventArgs args)
+        {
+            d.folderPaths = Folders;
+            d.CompareBy = new EnumerableCompareType[]
+            {
+                EnumerableCompareType.Name
+            };
+            d.CryptographyType = EnumerableCryptographType.Md5;
+            d.FileTypeFilter = new string[] { "*" };
+            
+            d.Starting += async (s, e) =>
+            {
+                await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    this.tblTotalFiles.Text = d.TotalFiles.ToString();
+                });
+            };
+            int count = 1;
+            d.CompletedOneFile += async (s, e) =>
+            {
+                await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    this.tblProgress.Text = count.ToString();
+                });
+                count++;
+            };
+
+            d.Completed += async (s, e) =>
+            {
+                await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                {
+                    await new MessageDialog("Completed!").ShowAsync();
+                    this.cvs.Source = from list in Group
+                                      from record in list.records
+                                      group record by list.TypeGroup;
+                });
+            };
+            
+            Group = await Task.Run(() => d.Execute());
+        }
+
+        private bool CheckFolderExist(
+            ObservableCollection<StorageFolder> folders, 
+            StorageFolder newFolder)
+        {
+            foreach(var folder in folders)
+            {
+                if (folder.Path.Equals(newFolder.Path))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public ObservableCollection<GroupRecord> Group
+        {
+            get
+            {
+                return group;
+            }
+            set
+            {
+                if (value != this.group)
+                {
+                    this.group = value;
+                    NotifyPropertyChanged("Group");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void btnDetectOrigin_Click(object sender, RoutedEventArgs e)
+        {
+            detectOriginOption = new EnumerableDetectOrigin[] {
+                (EnumerableDetectOrigin)cbCriteral.SelectedItem,
+                EnumerableDetectOrigin.ShortestName
+            };
+            d.DetectOriginRecords(Group, detectOriginOption);
+
+        }
+
+        private async void btnExportOutput_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            var file = await folder.CreateFileAsync(outputFileName + ".txt", 
+                CreationCollisionOption.ReplaceExisting);
+
+            var stream = await file.OpenAsync(FileAccessMode.ReadWrite);
+            using (var outputStream = stream.GetOutputStreamAt(0))
+            {
+                using (var dataWriter = new Windows.Storage.Streams.DataWriter(outputStream))
+                {
+                    //---Input:{số folder được chọn}
+                    dataWriter.WriteString(String.Format("---Input:{0}\r\n", Folders.Count));
+
+                    //{Đường dẫn folder}
+                    foreach(var item in Folders)
+                    {
+                        dataWriter.WriteString(item.Path + "\r\n");
+                    }
+                    dataWriter.WriteString("\r\n");
+                    
+                    //Criteral={Criteral Option}
+                    dataWriter.WriteString(String.Format("Criteral={0}\r\n", d.CompareBy.ToString()));
+
+                    //FileType={Định dạng file. Ex: .txt, .png, .pdf, ...}
+                    dataWriter.WriteString(String.Format("FileType={0}\r\n", d.FileTypeFilter));
+
+                    //DetectOrigin={Origin Option}
+                    dataWriter.WriteString(String.Format("DetectOrigin={0}\r\n\r\n", detectOriginOption[0].ToString()));
+
+                    //---Output:{số nhóm file trùng nhau}
+                    dataWriter.WriteString(String.Format("---Output:{0}\r\n", Group.Count));
+                    if(Group.Count == 0)
+                    {
+                        dataWriter.WriteString("\r\n");
+                    }
+
+                    //{Flag} {Đường dẫn của file}
+                    foreach(var item in Group)
+                    {
+                        foreach(var record in item.records)
+                        {
+                            if(record.IsOrigin == true)
+                            {
+                                dataWriter.WriteString(String.Format("{0} {1}\r\n", 0, record.Path));
+                            }
+                        }
+                        foreach (var record in item.records)
+                        {
+                            if (record.IsOrigin == false)
+                            {
+                                dataWriter.WriteString(String.Format("{0} {1}\r\n", 1, record.Path));
+                            }
+                        }
+                        dataWriter.WriteString("\r\n");
+                    }
+                    
+                    //---end
+                    dataWriter.WriteString("---end");
+
+                    await dataWriter.StoreAsync();
+                }
+            }
+            stream.Dispose();
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            d = new DuplicateDetector();
+            outputFileName = String.Empty;
+            Folders = new ObservableCollection<StorageFolder>();
+            group = new ObservableCollection<GroupRecord>();
+            //option = EnumerableDetectOrigin.NewestFile;
+            lvDuplicate.ItemsSource = null;
+            tblTotalFiles.Text = "0";
+            tblProgress.Text = "0";
+
+        }
+
+        private void cbCompareBy_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void cbCriteral_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
